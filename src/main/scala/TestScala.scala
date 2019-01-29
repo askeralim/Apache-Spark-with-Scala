8aa5badcb93a87351573c2fd75d483a815cb6880
Askerali Maruthullathil <askeralim@gmail.com>
2019-01-29 13:07:28 +0300

    File CSV Read

--- src/main/scala/TestScala.scala
+++ src/main/scala/TestScala.scala
@@ -1,5 +1,15 @@
+import scala.io.Source
+
 object TestScala extends App {
   override def main(args: Array[String]): Unit = {
-    println("Hello World")
+    // val field=2
+    for (field <- 0 to 3) {
+      val deviceList = Source.fromFile("../../Resources/SocialData/DeviceTypeList.csv").getLines().map(x => x.split(",")).map(fields => fields(field).trim()).toList
+      val prodDeviceList = Source.fromFile("../../Resources/SocialData/export.csv").getLines().map(x => x.split(",")).map(fields => fields(field).trim()).toList
+      //prodDeviceList.foreach(println)
+      prodDeviceList.filter(dev => !deviceList.contains(dev)).foreach(println)
+      println("-------- END ---------")
+      //println(prodDeviceList.contains("sagem VDSL"))
+    }
   }
 }
